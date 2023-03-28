e4f32602745f1097168204262fbc982bafdb5eba
rafalmaziejuk <rafalmaziejuk@gmail.com>
2023-03-28 20:43:12 +0200

    Implement assertion macros
    Signed-off-by: rafalmaziejuk <rafalmaziejuk@gmail.com>

--- src/utilities/logging/log_categories.cpp
+++ src/utilities/logging/log_categories.cpp
@@ -1,1 +1,3 @@
 #include "utilities/logging/log_categories.h"
+
+NOX_DEFINE_LOG_CATEGORY(DEBUG);
--- src/utilities/string_helper.h
+++ src/utilities/string_helper.h
@@ -3,15 +3,11 @@
 #include <algorithm>
 #include <string>
 
-namespace NOX {
-
-namespace StringHelper {
+namespace NOX::StringHelper {
 
 inline void toLower(std::string &value) {
     const auto customToLower = [](uint8_t c) { return static_cast<uint8_t>(std::tolower(c)); };
     std::transform(value.begin(), value.end(), value.begin(), customToLower);
 }
 
-} // namespace StringHelper
-
-} // namespace NOX
+} // namespace NOX::StringHelper
