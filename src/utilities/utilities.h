775d75c75e26068b65403defe5a23e64d5685161
Rafal Maziejuk <46377597+rafalmaziejuk@users.noreply.github.com>
2023-04-04 21:11:22 +0200

    Fix clang-tidy issues (#18)
    Signed-off-by: rafalmaziejuk <rafalmaziejuk@gmail.com>

--- src/utilities/utilities.h
+++ src/utilities/utilities.h
@@ -6,7 +6,7 @@
 namespace NOX::Utilities {
 
 inline std::string toLower(std::string value) {
-    const auto customToLower = [](uint8_t c) { return static_cast<uint8_t>(std::tolower(c)); };
+    const auto customToLower = [](uint8_t character) { return static_cast<uint8_t>(std::tolower(character)); };
     std::transform(value.begin(), value.end(), value.begin(), customToLower);
     return value;
 }
