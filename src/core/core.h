bfcb1290d29e0a2be7f43e0b3a56c21774411054
Rafal Maziejuk <46377597+rafalmaziejuk@users.noreply.github.com>
2023-04-08 04:36:30 +0200

    Define per verbosity logging macros to avoid defines conflicts (#24)
    Signed-off-by: rafalmaziejuk <rafalmaziejuk@gmail.com>

---
+++ src/core/core.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#if defined(NOX_STATIC)
+inline constexpr auto isStaticLinking = true;
+#else
+inline constexpr auto isStaticLinking = false;
+#endif
+
+#if defined(NOX_DEBUG)
+inline constexpr auto isDebugConfiguration = true;
+#else
+inline constexpr auto isDebugConfiguration = false;
+#endif
--- src/utilities/logger.h
+++ src/utilities/logger.h
@@ -5,79 +5,34 @@
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 
-#define NOX_DECLARE_LOG_CATEGORY_EXTERN(categoryName, defaultVerbosity)                              \
-    extern struct LogCategory##categoryName : public NOX::LogCategory {                              \
-        LogCategory##categoryName() : LogCategory(#categoryName, NOX::Verbosity::defaultVerbosity) { \
-            NOX::Logger::instance().registerCategory(*this);                                         \
-        }                                                                                            \
-    } categoryName;
+#define NOX_LOG_IMPL(verbosity, category, message, ...) \
+    Logger::instance().getLogger().verbosity("[" #category "] " message, __VA_ARGS__)
 
-#define NOX_DEFINE_LOG_CATEGORY(categoryName) \
-    LogCategory##categoryName categoryName{};
-
-#define NOX_LOG(categoryName, verbosity, ...) \
-    NOX::Logger::instance().log<NOX::Verbosity::verbosity>(categoryName, __VA_ARGS__)
+#define NOX_LOG_TRACE(category, message, ...) NOX_LOG_IMPL(trace, category, message, __VA_ARGS__)
+#define NOX_LOG_DEBUG(category, message, ...) NOX_LOG_IMPL(debug, category, message, __VA_ARGS__)
+#define NOX_LOG_INFO(category, message, ...) NOX_LOG_IMPL(info, category, message, __VA_ARGS__)
+#define NOX_LOG_WARN(category, message, ...) NOX_LOG_IMPL(warn, category, message, __VA_ARGS__)
+#define NOX_LOG_ERROR(category, message, ...) NOX_LOG_IMPL(error, category, message, __VA_ARGS__)
+#define NOX_LOG_CRITICAL(category, message, ...) NOX_LOG_IMPL(critical, category, message, __VA_ARGS__)
 
 namespace NOX {
 
-enum class Verbosity {
-    TRACE,
-    DEBUG,
-    INFO,
-    WARN,
-    ERROR,
-    CRITICAL
-};
-
-struct LogCategory {
-    LogCategory(std::string_view name, Verbosity verbosity) : categoryName{name},
-                                                              defaultVerbosity{verbosity} {}
-
-    std::string categoryName;
-    Verbosity defaultVerbosity;
-};
-
 class NOX_EXPORT Logger : public Singleton<Logger> {
   public:
-    Logger();
+    Logger() {
+        auto consoleSink{std::make_shared<spdlog::sinks::stdout_color_sink_mt>()};
+        consoleSink->set_pattern("[%T]%^[%l][%n]%v%$");
 
-    void registerCategory(LogCategory &logCategory);
+        m_logger = std::make_unique<spdlog::logger>("NOX", consoleSink);
+        m_logger->set_level(spdlog::level::trace);
+    }
 
-    template <Verbosity verbosity, typename... Args>
-    void log(const LogCategory &logCategory, Args &&...args) {
-        switch (verbosity) {
-        case Verbosity::TRACE:
-            m_loggers[logCategory.categoryName]->trace(std::forward<Args>(args)...);
-            break;
-
-        case Verbosity::DEBUG:
-            m_loggers[logCategory.categoryName]->debug(std::forward<Args>(args)...);
-            break;
-
-        case Verbosity::INFO:
-            m_loggers[logCategory.categoryName]->info(std::forward<Args>(args)...);
-            break;
-
-        case Verbosity::WARN:
-            m_loggers[logCategory.categoryName]->warn(std::forward<Args>(args)...);
-            break;
-
-        case Verbosity::ERROR:
-            m_loggers[logCategory.categoryName]->error(std::forward<Args>(args)...);
-            break;
-
-        case Verbosity::CRITICAL:
-            m_loggers[logCategory.categoryName]->critical(std::forward<Args>(args)...);
-            break;
-        }
+    spdlog::logger &getLogger() const {
+        return *m_logger;
     }
 
   private:
-    std::shared_ptr<spdlog::sinks::stdout_color_sink_mt> m_consoleSink{nullptr};
-    std::unordered_map<std::string, std::unique_ptr<spdlog::logger>> m_loggers{};
+    std::unique_ptr<spdlog::logger> m_logger{nullptr};
 };
 
 } // namespace NOX
-
-NOX_DECLARE_LOG_CATEGORY_EXTERN(DEBUG, ERROR);
-NOX_DECLARE_LOG_CATEGORY_EXTERN(PLUGINS, TRACE);
