5b8abd6776ebbe00e06b3a20401d2e484d806ac1
Rafal Maziejuk <46377597+rafalmaziejuk@users.noreply.github.com>
2023-04-17 21:44:12 +0200

    Choose renderer API through renderer descriptor (#35)
    Signed-off-by: rafalmaziejuk <rafalmaziejuk@gmail.com>

--- include/nox/renderer/renderer.h
+++ include/nox/renderer/renderer.h
@@ -2,19 +2,23 @@
 
 #include <nox/export.h>
 #include <nox/non_copyable.h>
+#include <nox/renderer/common_types.h>
 
 #include <functional>
 #include <memory>
-#include <string_view>
 
 namespace NOX {
 
 class Renderer;
 using RendererDeleter = std::function<void(Renderer *)>;
 
+struct RendererDescriptor {
+    RendererAPI api;
+};
+
 class NOX_EXPORT Renderer : public NonCopyable {
   public:
-    [[nodiscard]] static std::unique_ptr<Renderer, RendererDeleter> create(std::string_view rendererName);
+    [[nodiscard]] static std::unique_ptr<Renderer, RendererDeleter> create(const RendererDescriptor &descriptor);
 };
 
 } // namespace NOX
