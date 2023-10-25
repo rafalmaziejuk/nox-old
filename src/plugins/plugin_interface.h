59d76a01e5b4e3599485460bc907787a3e51ce1f
Rafal Maziejuk <46377597+rafalmaziejuk@users.noreply.github.com>
2023-10-26 00:21:08 +0200

    [NOX-27] load renderer plugin only when requested (#96)
    Signed-off-by: rafalmaziejuk <rafalmaziejuk@gmail.com>

--- src/plugins/plugin_interface.h
+++ src/plugins/plugin_interface.h
@@ -27,11 +27,11 @@
 #define NOX_PLUGIN_VERSION(pluginName) \
     void StaticPlugin##pluginName::setPluginVersion() { m_version = NOX_PLUGIN_API_VERSION; }
 
-#define NOX_PLUGIN_LOAD(pluginName)                                                                         \
-    NOX_PLUGIN_VISIBILITY std::unique_ptr<NOX::Plugin> createPlugin##pluginName();                          \
-    namespace NOX::pluginName##Plugin {                                                                     \
-        const bool pluginRegistered = PluginManager::instance().registerPlugin(createPlugin##pluginName()); \
-    }
+#define NOX_PLUGIN_IMPORT(pluginName) \
+    NOX_PLUGIN_VISIBILITY std::unique_ptr<NOX::Plugin> createPlugin##pluginName()
+
+#define NOX_PLUGIN_LOAD(pluginName) \
+    PluginManager::instance().registerPlugin(createPlugin##pluginName())
 #endif
 
 #if !defined(NOX_STATIC)
@@ -45,8 +45,8 @@
 #define NOX_PLUGIN_VERSION(pluginName) \
     NOX_PLUGIN_VISIBILITY NOX_PLUGIN_EXPORT uint8_t pluginVersion() { return NOX_PLUGIN_API_VERSION; }
 
-#define NOX_PLUGIN_LOAD(pluginName)                                                                                                                    \
-    namespace NOX::pluginName##Plugin {                                                                                                                \
-        const bool pluginRegistered = PluginManager::instance().registerPlugin(DynamicPlugin::create(#pluginName, createPluginFilenameWithExtension)); \
-    }
+#define NOX_PLUGIN_IMPORT(pluginName)
+
+#define NOX_PLUGIN_LOAD(pluginName) \
+    PluginManager::instance().registerPlugin(DynamicPlugin::create(#pluginName, createPluginFilenameWithExtension))
 #endif
